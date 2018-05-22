af301c9af2882cb9022bdda24d573dd96b75067c
Noora41 <nooeachy20@gmail.com>
2018-05-22 16:55:44 +0600

    git commit

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -1,6 +1,27 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-    <h1>Welcome</h1>
-    <p>This is my application in Laravel.</p> 
-@endsection    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Style -->
+    <link href="{{asset('public/css/app.css') }}" rel="stylesheet">
+    <link href="{{asset('front-end/css/style.css')}}"  rel="stylesheet">
+    <link href="{{asset('front-end/css/bootstrap.css')}}" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href='https://fonts.googleapis.com/css?family=ABeeZee'   rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">  
+
+</head>
+<body>
+   
+</body>
+</html>    
